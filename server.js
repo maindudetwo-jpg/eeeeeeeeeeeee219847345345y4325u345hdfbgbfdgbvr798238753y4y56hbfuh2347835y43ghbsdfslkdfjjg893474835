82afa6fd741f5e6c25d8ab3947bb68f0592eeccb
maindudetwo-jpg <maindudetwo@gmail.com>
2026-06-01 14:30:44 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ res.sendFile(path.join(__dirname, "index.html"));
 app.post("/ping", async (req, res) => {
 const { ip } = req.body;
 
+```
 console.log("Received ping request for:", ip);
 
 try {
@@ -37,3 +38,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 console.log("Server running on port " + port);
 });
+
