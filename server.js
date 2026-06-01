8a941efe2e1f73e9b19afbcf97204cb674e833bc
maindudetwo-jpg <maindudetwo@gmail.com>
2026-06-01 14:10:36 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const path = require("path");
-
-console.log("Starting application...");
-
-let ping;
-
-try {
-ping = require("ping");
-console.log("Ping module loaded.");
-} catch (err) {
-console.error("Failed to load ping module:", err);
-process.exit(1);
-}
+const ping = require("ping");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-const file = path.join(__dirname, "index.html");
-console.log("Serving:", file);
-res.sendFile(file);
+res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/ping", async (req, res) => {
